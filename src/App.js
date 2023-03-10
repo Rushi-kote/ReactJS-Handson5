c6da26320007220640ebbb2f35260393fca8ef66
Rushi-kote <Koterushi@gmail.com>
2023-03-10 18:39:34 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css"
+import ButtonComponent from './components/ButtonComponent';
+import MouseComponent from './components/MouseComponent';
+
+function App() {
+  return (
+    <div className='flexContainer'>
+      <ButtonComponent/>
+      <MouseComponent/>
+    </div>
+  );
+}
+
+export default App;
